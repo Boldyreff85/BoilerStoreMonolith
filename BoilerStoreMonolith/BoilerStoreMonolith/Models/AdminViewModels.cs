9e6bd0a59df9e77ee0807d362d09d3b752859b22
Vitaly Udenkov <vitalisun@gmail.com>
2019-05-14 13:03:46 +0300

    добавил модели представлений для admin контроллера

---
+++ BoilerStoreMonolith/BoilerStoreMonolith/Models/AdminViewModels.cs
@@ -0,0 +1,16 @@
+﻿using BoilerStoreMonolith.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BoilerStoreMonolith.Models
+{
+    public enum ImgType { ProductImg, CategoryImg, FirmImg };
+
+    public class AdminEditViewModel
+    {
+        public Product Product { get; set; }
+        public ImgType ImageToLoad { get; set; }
+    }
+}
