3afe31c1aa0bf811b734ce836ca9d39a081047cc
aleksey-t <megapozitivizm@yandex.ru>
2019-06-25 12:18:36 +0300

    small fixes

--- BoilerStoreMonolith/BoilerStoreMonolith/Scripts/load-content.js
+++ BoilerStoreMonolith/BoilerStoreMonolith/Scripts/load-content.js
@@ -1,35 +1,47 @@
-﻿$(function() {
-  var $catalogue = $(".catalogue");
-  var $links = $catalogue.find("a");
-  var prevHistoryState = "";
+﻿// Load content
+$(function () {
+    var $catalogue = $(".catalogue");
+    var $links = $catalogue.find("a");
+    var prevHistoryState = "";
 
-  $links.each(function(link) {
-    $links[link].addEventListener("click", function(e) {
-      e.preventDefault();
+    $links.each(function (link) {
+        $links[link].addEventListener("click", function (e) {
+            e.preventDefault();
 
-      if (prevHistoryState !== this.href) {
-        prevHistoryState = this.href;
-        history.pushState(this.href, this.href, this.href);
-        getContent(this.href);
-      }
+            if (prevHistoryState !== this.href) {
+                prevHistoryState = this.href;
+                history.pushState(this.href, this.href, this.href);
+                getContent(this.href);
+            }
+        });
     });
-  });
 
-  function getContent(url) {
-    $.ajax({
-      type: "POST",
-      url: url,
-      data: { isAjax: true }
-    }).done(function(data) {
-      $("#page-content").html(data);
-    });
-  }
-
-  window.addEventListener("popstate", function(e) {
-    if (!e.state) {
-      window.location = "/";
-    } else {
-      getContent(e.state);
+    function getContent(url) {
+        $.ajax({
+            type: "POST",
+            url: url,
+            data: { isAjax: true }
+        }).done(function (data) {
+            $("#page-content").html(data);
+        });
     }
-  });
+
+    window.addEventListener("popstate", function (e) {
+        if (!e.state) {
+            window.location = "/";
+        } else {
+            getContent(e.state);
+        }
+    });
 });
+
+// Hide sidebar on home page
+$(
+    function () {
+        console.log(1)
+        if (window.location.pathname !== "/") {
+            $($('#sidebar-menu')[0]).show();
+        }
+    }
+);
+
